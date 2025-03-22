c7e5811bf03c37c9444d4132f654e59615450f75
ArseniyDev <jkl@usa.com>
2025-03-22 18:57:28 +0200

    Add .gitignore and update store-plugin structure without modification to functionality

--- store-plugin/src/exports/rsc.ts
+++ store-plugin/src/exports/rsc.ts
@@ -1,2 +1,5 @@
 export { Actions } from '../components/actions'
 export { PluginIcon } from '../components/plugin-icon'
+export { PluginListView } from "../components/plugin-list";
+export { PluginEditView } from "../components/plugin-edit";
+
