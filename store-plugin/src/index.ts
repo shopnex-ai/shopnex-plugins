741e48eb67509311f4619533a6a5b4e3c9bfc7b0
ArseniyDev <jkl@usa.com>
2025-03-22 18:58:08 +0200

    Initialize store-plugin structure in index.ts for improved organization and future enhancements

--- store-plugin/src/index.ts
+++ store-plugin/src/index.ts
@@ -1,8 +1,4 @@
 import type { CollectionSlug, Config } from 'payload'
-
-import { stripePlugin } from '@payloadcms/plugin-stripe'
-import { cjPlugin } from '@shoplyjs/cj-plugin'
-
 import { PluginStore } from './plugin-store'
 
 export type StorePluginConfig = {
@@ -20,23 +16,6 @@ export const storePlugin =
 
     config.collections.push(PluginStore)
 
-    if (pluginOptions.collections) {
-      for (const collectionSlug in pluginOptions.collections) {
-        const collection = config.collections.find(
-          (collection) => collection.slug === collectionSlug,
-        )
-
-        if (collection) {
-          collection.fields.push({
-            name: 'addedByPlugin',
-            type: 'text',
-            admin: {
-              position: 'sidebar',
-            },
-          })
-        }
-      }
-    }
 
     /**
      * If the plugin is disabled, we still want to keep added collections/fields so the database schema is consistent which is important for migrations.
@@ -62,48 +41,6 @@ export const storePlugin =
       config.admin.components.beforeDashboard = []
     }
 
-    config.plugins?.push(
-      cjPlugin({
-        cjApiKey: process.env.CJ_API_KEY || '',
-        cjEmailAddress: process.env.CJ_EMAIL_ADDRESS || '',
-      }),
-    )
-
-    const incomingOnInit = config.onInit
-
-    config.onInit = async (payload) => {
-      // Ensure we are executing any existing onInit functions before running our own.
-      if (incomingOnInit) {
-        await incomingOnInit(payload)
-      }
-
-      const plugins = [
-        { name: 'stripe-plugin', displayName: 'Stripe', plugin: stripePlugin, shortName: 'stripe' },
-        { name: 'cj-plugin', displayName: 'CJ Dropshipping', plugin: undefined, shortName: 'cj' },
-      ]
-
-      // for (const { name: pluginName, displayName } of plugins) {
-      //   const { totalDocs } = await payload.count({
-      //     collection: 'plugins-store',
-      //     where: {
-      //       pluginName: {
-      //         equals: pluginName,
-      //       },
-      //     },
-      //   })
-
-      //   if (totalDocs === 0) {
-      //     await payload.create({
-      //       collection: 'plugins-store',
-      //       data: {
-      //         displayName,
-      //         pluginName,
-      //         pluginVersion: '1.0.0',
-      //       },
-      //     })
-      //   }
-      // }
-    }
 
     return config
   }
