4c5a33e4c4cd788efed2809ec761d36ff7d40de6
ArseniyDev <jkl@usa.com>
2025-03-24 21:52:08 +0200

    Clean up and organize files in cj-plugin for better maintainability and to streamline future development efforts

--- cj-plugin/src/cj-settings.ts
+++ cj-plugin/src/cj-settings.ts
@@ -1,48 +1,52 @@
-import type { GlobalConfig } from 'payload'
+import type { GlobalConfig } from "payload";
 
-// import { admins } from '@/access/roles'
-
-import { syncProducts } from './service/sync-products'
+import { syncProducts } from "./service/sync-products";
 
 export const CjSettings: GlobalConfig = {
-  slug: 'cj-settings',
-  access: {
-    read: () => true,
-    update: ({ req }) => !!req.user?.roles?.includes('admin'),
-  },
-  admin: {
-    group: 'Plugins',
-  },
-  fields: [
-    {
-      name: 'items',
-      type: 'array',
-      admin: {
-        description: 'A list of product URLs to sync with CJ Dropshipping',
-      },
-      fields: [
-        {
-          name: 'productUrl',
-          type: 'text',
-        },
-      ],
-      label: 'Products',
-      labels: {
-        plural: 'Product URLs',
-        singular: 'Product URL',
-      },
+    slug: "cj-settings",
+    access: {
+        read: () => true,
+        update: ({ req }) => !!req.user?.roles?.includes("admin"),
     },
-  ],
-  hooks: {
-    afterChange: [
-      async ({ doc, req: { payload } }) => {
-        const productIds = doc.items.map((item: any) => {
-          const match = item.productUrl.match(/(?<=-p-)([0-9A-Fa-f-]+)(?=\.html)/)
-          return match ? match[0] : null
-        })
-        await syncProducts(productIds, payload)
-      },
+    admin: {
+        group: "Plugins",
+    },
+    fields: [
+        {
+            label: "Logo Area POD",
+            name: "pod",
+            type: "upload",
+            relationTo: "media",
+        },
+        {
+            name: "items",
+            type: "array",
+            admin: {
+                description: "A list of product URLs to sync with CJ Dropshipping",
+            },
+            fields: [
+                {
+                    name: "productUrl",
+                    type: "text",
+                },
+            ],
+            label: "Products",
+            labels: {
+                plural: "Product URLs",
+                singular: "Product URL",
+            },
+        },
     ],
-  },
-  label: 'CJ Dropshipping',
-}
+    hooks: {
+        afterChange: [
+            async ({ doc, req: { payload } }) => {
+                const productIds = doc.items.map((item: any) => {
+                    const match = item.productUrl.match(/(?<=-p-)([0-9A-Fa-f-]+)(?=\.html)/);
+                    return match ? match[0] : null;
+                });
+                await syncProducts(productIds, payload);
+            },
+        ],
+    },
+    label: "CJ Dropshipping",
+};
