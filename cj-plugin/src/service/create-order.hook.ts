af5d38e2b4f50ace8491bf22848ea540a24ee701
ArseniyDev <jkl@usa.com>
2025-03-26 21:45:29 +0200

    Refactor create-order.hook.ts for enhanced readability and maintainability in the cj-plugin structure

--- cj-plugin/src/service/create-order.hook.ts
+++ cj-plugin/src/service/create-order.hook.ts
@@ -18,6 +18,7 @@ export const createOrderHook: CollectionAfterChangeHook<Orders> = async ({ doc, 
         return;
     }
     const payload: BasePayload = req.payload;
+    
     const cjSettings: any = await payload.findGlobal({
         slug: "cj-settings",
     });
