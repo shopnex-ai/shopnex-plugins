6b75e1b9442512271bf1c4b2fe5912e9dda5e171
ArseniyDev <jkl@usa.com>
2025-03-31 08:58:44 +0300

    fix issue with json invoke undefined

--- auth-plugin/src/core/protocols/passkey/index.ts
+++ auth-plugin/src/core/protocols/passkey/index.ts
@@ -1,26 +1,26 @@
-import { PayloadRequest } from "payload"
-import { MissingEmailAPIError } from "../../errors/apiErrors"
-import { hashCode } from "../../utils/hash"
+import { PayloadRequest } from "payload";
+import { MissingEmailAPIError } from "../../errors/apiErrors";
+import { hashCode } from "../../utils/hash";
 
 export async function InitPasskey(request: PayloadRequest): Promise<Response> {
-  const { data } = (await request.json()) as { data: { email: string } }
+    const { data } = (await request.json?.()) as { data: { email: string } };
 
-  if (!data.email) {
-    throw new MissingEmailAPIError()
-  }
+    if (!data.email) {
+        throw new MissingEmailAPIError();
+    }
 
-  const existingRecord = await request.payload.find({
-    collection: "accounts",
-    where: {
-      sub: {
-        equals: hashCode(data.email + request.payload.secret).toString(),
-      },
-    },
-  })
-  if (existingRecord.totalDocs !== 1) {
-    return new Response(JSON.stringify({ data: {} }), { status: 200 })
-  }
-  return new Response(JSON.stringify({ data: existingRecord.docs[0] }), {
-    status: 200,
-  })
+    const existingRecord = await request.payload.find({
+        collection: "accounts",
+        where: {
+            sub: {
+                equals: hashCode(data.email + request.payload.secret).toString(),
+            },
+        },
+    });
+    if (existingRecord.totalDocs !== 1) {
+        return new Response(JSON.stringify({ data: {} }), { status: 200 });
+    }
+    return new Response(JSON.stringify({ data: existingRecord.docs[0] }), {
+        status: 200,
+    });
 }
--- auth-plugin/src/core/protocols/password.ts
+++ auth-plugin/src/core/protocols/password.ts
@@ -1,354 +1,340 @@
-import { getCookieExpiration, parseCookies, PayloadRequest } from "payload"
+import { getCookieExpiration, parseCookies, PayloadRequest } from "payload";
 import {
-  AuthenticationFailed,
-  EmailAlreadyExistError,
-  InvalidCredentials,
-  InvalidRequestBodyError,
-  UnauthorizedAPIRequest,
-  UserNotFoundAPIError,
-} from "../errors/apiErrors"
-import { hashPassword, verifyPassword } from "../utils/password"
-import { SuccessKind } from "../../types"
-import { ephemeralCode, verifyEphemeralCode } from "../utils/hash"
-import { EPHEMERAL_CODE_COOKIE_NAME } from "../../constants"
+    AuthenticationFailed,
+    EmailAlreadyExistError,
+    InvalidCredentials,
+    InvalidRequestBodyError,
+    UnauthorizedAPIRequest,
+    UserNotFoundAPIError,
+} from "../errors/apiErrors";
+import { hashPassword, verifyPassword } from "../utils/password";
+import { SuccessKind } from "../../types";
+import { ephemeralCode, verifyEphemeralCode } from "../utils/hash";
+import { EPHEMERAL_CODE_COOKIE_NAME } from "../../constants";
 import {
-  createSessionCookies,
-  invalidateSessionCookies,
-  verifySessionCookie,
-} from "../utils/cookies"
-import { revokeSession } from "../utils/session"
+    createSessionCookies,
+    invalidateSessionCookies,
+    verifySessionCookie,
+} from "../utils/cookies";
+import { revokeSession } from "../utils/session";
 
 export const PasswordSignin = async (
-  request: PayloadRequest,
-  internal: {
-    usersCollectionSlug: string
-  },
-  sessionCallBack: (user: { id: string; email: string }) => Promise<Response>,
-) => {
-  const body =
-    request.json &&
-    ((await request.json()) as { email: string; password: string })
-
-  if (!body?.email || !body.password) {
-    return new InvalidRequestBodyError()
-  }
-
-  const { payload } = request
-  const { docs } = await payload.find({
-    collection: internal.usersCollectionSlug,
-    where: {
-      email: { equals: body.email },
+    request: PayloadRequest,
+    internal: {
+        usersCollectionSlug: string;
     },
-    limit: 1,
-  })
+    sessionCallBack: (user: { id: string; email: string }) => Promise<Response>,
+) => {
+    const body = request.json && ((await request.json?.()) as { email: string; password: string });
 
-  if (docs.length !== 1) {
-    return new UserNotFoundAPIError()
-  }
+    if (!body?.email || !body.password) {
+        return new InvalidRequestBodyError();
+    }
 
-  const user = docs[0]
-  const isVerifed = await verifyPassword(
-    body.password,
-    user["hashedPassword"],
-    user["salt"],
-    user["hashIterations"],
-  )
-  if (!isVerifed) {
-    return new InvalidCredentials()
-  }
-  return sessionCallBack({
-    id: user.id as string,
-    email: body.email,
-  })
-}
+    const { payload } = request;
+    const { docs } = await payload.find({
+        collection: internal.usersCollectionSlug,
+        where: {
+            email: { equals: body.email },
+        },
+        limit: 1,
+    });
+
+    if (docs.length !== 1) {
+        return new UserNotFoundAPIError();
+    }
+
+    const user = docs[0];
+    const isVerifed = await verifyPassword(
+        body.password,
+        user["hashedPassword"],
+        user["salt"],
+        user["hashIterations"],
+    );
+    if (!isVerifed) {
+        return new InvalidCredentials();
+    }
+    return sessionCallBack({
+        id: user.id as string,
+        email: body.email,
+    });
+};
 
 export const PasswordSignup = async (
-  request: PayloadRequest,
-  internal: {
-    usersCollectionSlug: string
-  },
-  sessionCallBack: (user: { id: string; email: string }) => Promise<Response>,
+    request: PayloadRequest,
+    internal: {
+        usersCollectionSlug: string;
+    },
+    sessionCallBack: (user: { id: string; email: string }) => Promise<Response>,
 ) => {
-  const body =
-    request.json &&
-    ((await request.json()) as {
-      email: string
-      password: string
-      allowAutoSignin?: boolean
-      profile?: Record<string, unknown>
-    })
+    const body =
+        request.json &&
+        ((await request.json?.()) as {
+            email: string;
+            password: string;
+            allowAutoSignin?: boolean;
+            profile?: Record<string, unknown>;
+        });
 
-  if (!body?.email || !body.password) {
-    return new InvalidRequestBodyError()
-  }
+    if (!body?.email || !body.password) {
+        return new InvalidRequestBodyError();
+    }
 
-  const { payload } = request
-  const { docs } = await payload.find({
-    collection: internal.usersCollectionSlug,
-    where: {
-      email: { equals: body.email },
-    },
-    limit: 1,
-  })
+    const { payload } = request;
+    const { docs } = await payload.find({
+        collection: internal.usersCollectionSlug,
+        where: {
+            email: { equals: body.email },
+        },
+        limit: 1,
+    });
 
-  if (docs.length > 0) {
-    return new EmailAlreadyExistError()
-  }
+    if (docs.length > 0) {
+        return new EmailAlreadyExistError();
+    }
 
-  const {
-    hash: hashedPassword,
-    salt,
-    iterations,
-  } = await hashPassword(body.password)
+    const { hash: hashedPassword, salt, iterations } = await hashPassword(body.password);
 
-  const user = await payload.create({
-    collection: internal.usersCollectionSlug,
-    data: {
-      email: body.email,
-      hashedPassword: hashedPassword,
-      hashIterations: iterations,
-      salt,
-      ...body.profile,
-    },
-  })
+    const user = await payload.create({
+        collection: internal.usersCollectionSlug,
+        data: {
+            email: body.email,
+            hashedPassword: hashedPassword,
+            hashIterations: iterations,
+            salt,
+            ...body.profile,
+        },
+    });
 
-  if (body.allowAutoSignin) {
-    return sessionCallBack({
-      id: user.id as string,
-      email: body.email,
-    })
-  }
+    if (body.allowAutoSignin) {
+        return sessionCallBack({
+            id: user.id as string,
+            email: body.email,
+        });
+    }
 
-  return Response.json(
-    {
-      message: "Signed up successfully",
-      kind: SuccessKind.Created,
-      isSuccess: true,
-      isError: false,
-    },
-    { status: 201 },
-  )
-}
+    return Response.json(
+        {
+            message: "Signed up successfully",
+            kind: SuccessKind.Created,
+            isSuccess: true,
+            isError: false,
+        },
+        { status: 201 },
+    );
+};
 
 export const ForgotPasswordInit = async (
-  request: PayloadRequest,
-  internal: {
-    usersCollectionSlug: string
-  },
-) => {
-  const { payload } = request
-
-  const body =
-    request.json &&
-    ((await request.json()) as {
-      email: string
-    })
-
-  if (!body?.email) {
-    return new InvalidRequestBodyError()
-  }
-
-  const { docs } = await payload.find({
-    collection: internal.usersCollectionSlug,
-    where: {
-      email: { equals: body.email },
+    request: PayloadRequest,
+    internal: {
+        usersCollectionSlug: string;
     },
-    limit: 1,
-  })
+) => {
+    const { payload } = request;
 
-  if (docs.length !== 1) {
-    return new UserNotFoundAPIError()
-  }
-  const { code, hash } = await ephemeralCode(6, payload.secret)
+    const body =
+        request.json &&
+        ((await request.json?.()) as {
+            email: string;
+        });
 
-  await payload.sendEmail({
-    to: body.email,
-    subject: "Password recovery",
-    text: "Password recovery code: " + code,
-  })
+    if (!body?.email) {
+        return new InvalidRequestBodyError();
+    }
 
-  const res = new Response(
-    JSON.stringify({
-      message: "Password recovery initiated successfully",
-      kind: SuccessKind.Created,
-      isSuccess: true,
-      isError: false,
-    }),
-    { status: 201 },
-  )
-  const tokenExpiration = getCookieExpiration({
-    seconds: 300,
-  })
-  res.headers.append(
-    "Set-Cookie",
-    `${EPHEMERAL_CODE_COOKIE_NAME}=${hash};Path=/;HttpOnly;Secure=true;SameSite=lax;Expires=${tokenExpiration.toUTCString()}`,
-  )
-  return res
-}
+    const { docs } = await payload.find({
+        collection: internal.usersCollectionSlug,
+        where: {
+            email: { equals: body.email },
+        },
+        limit: 1,
+    });
+
+    if (docs.length !== 1) {
+        return new UserNotFoundAPIError();
+    }
+    const { code, hash } = await ephemeralCode(6, payload.secret);
+
+    await payload.sendEmail({
+        to: body.email,
+        subject: "Password recovery",
+        text: "Password recovery code: " + code,
+    });
+
+    const res = new Response(
+        JSON.stringify({
+            message: "Password recovery initiated successfully",
+            kind: SuccessKind.Created,
+            isSuccess: true,
+            isError: false,
+        }),
+        { status: 201 },
+    );
+    const tokenExpiration = getCookieExpiration({
+        seconds: 300,
+    });
+    res.headers.append(
+        "Set-Cookie",
+        `${EPHEMERAL_CODE_COOKIE_NAME}=${hash};Path=/;HttpOnly;Secure=true;SameSite=lax;Expires=${tokenExpiration.toUTCString()}`,
+    );
+    return res;
+};
 
 export const ForgotPasswordVerify = async (
-  request: PayloadRequest,
-  internal: {
-    usersCollectionSlug: string
-  },
+    request: PayloadRequest,
+    internal: {
+        usersCollectionSlug: string;
+    },
 ) => {
-  const { payload } = request
+    const { payload } = request;
 
-  const body =
-    request.json &&
-    ((await request.json()) as {
-      email: string
-      password: string
-      code: string
-    })
+    const body =
+        request.json &&
+        ((await request.json?.()) as {
+            email: string;
+            password: string;
+            code: string;
+        });
 
-  if (!body?.email || !body?.password || !body.code) {
-    return new InvalidRequestBodyError()
-  }
+    if (!body?.email || !body?.password || !body.code) {
+        return new InvalidRequestBodyError();
+    }
 
-  const cookies = parseCookies(request.headers)
-  const hash = cookies.get(EPHEMERAL_CODE_COOKIE_NAME)
-  if (!hash) {
-    return new UnauthorizedAPIRequest()
-  }
+    const cookies = parseCookies(request.headers);
+    const hash = cookies.get(EPHEMERAL_CODE_COOKIE_NAME);
+    if (!hash) {
+        return new UnauthorizedAPIRequest();
+    }
 
-  const isVerified = await verifyEphemeralCode(body.code, hash, payload.secret)
+    const isVerified = await verifyEphemeralCode(body.code, hash, payload.secret);
 
-  if (!isVerified) {
-    return new AuthenticationFailed()
-  }
-  const { docs } = await payload.find({
-    collection: internal.usersCollectionSlug,
-    where: {
-      email: { equals: body.email },
-    },
-    limit: 1,
-  })
+    if (!isVerified) {
+        return new AuthenticationFailed();
+    }
+    const { docs } = await payload.find({
+        collection: internal.usersCollectionSlug,
+        where: {
+            email: { equals: body.email },
+        },
+        limit: 1,
+    });
 
-  if (docs.length !== 1) {
-    return new UserNotFoundAPIError()
-  }
+    if (docs.length !== 1) {
+        return new UserNotFoundAPIError();
+    }
 
-  const {
-    hash: hashedPassword,
-    salt,
-    iterations,
-  } = await hashPassword(body.password)
+    const { hash: hashedPassword, salt, iterations } = await hashPassword(body.password);
 
-  await payload.update({
-    collection: internal.usersCollectionSlug,
-    id: docs[0].id,
-    data: {
-      hashedPassword,
-      salt,
-      hashIterations: iterations,
-    },
-  })
+    await payload.update({
+        collection: internal.usersCollectionSlug,
+        id: docs[0].id,
+        data: {
+            hashedPassword,
+            salt,
+            hashIterations: iterations,
+        },
+    });
 
-  const res = new Response(
-    JSON.stringify({
-      message: "Password recovered successfully",
-      kind: SuccessKind.Updated,
-      isSuccess: true,
-      isError: false,
-    }),
-    { status: 201 },
-  )
-  res.headers.append(
-    "Set-Cookie",
-    `${EPHEMERAL_CODE_COOKIE_NAME}=;Path=/;HttpOnly;Secure=true;SameSite=lax;Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
-  )
-  return res
-}
+    const res = new Response(
+        JSON.stringify({
+            message: "Password recovered successfully",
+            kind: SuccessKind.Updated,
+            isSuccess: true,
+            isError: false,
+        }),
+        { status: 201 },
+    );
+    res.headers.append(
+        "Set-Cookie",
+        `${EPHEMERAL_CODE_COOKIE_NAME}=;Path=/;HttpOnly;Secure=true;SameSite=lax;Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+    );
+    return res;
+};
 
 export const ResetPassword = async (
-  cookieName: string,
-  secret: string,
-  internal: {
-    usersCollectionSlug: string
-  },
-  request: PayloadRequest,
+    cookieName: string,
+    secret: string,
+    internal: {
+        usersCollectionSlug: string;
+    },
+    request: PayloadRequest,
 ) => {
-  const { payload } = request
-  const cookies = parseCookies(request.headers)
-  const token = cookies.get(cookieName)
-  if (!token) {
-    return new UnauthorizedAPIRequest()
-  }
+    const { payload } = request;
+    const cookies = parseCookies(request.headers);
+    const token = cookies.get(cookieName);
+    if (!token) {
+        return new UnauthorizedAPIRequest();
+    }
 
-  const jwtResponse = await verifySessionCookie(token, secret)
-  if (!jwtResponse.payload) {
-    return new UnauthorizedAPIRequest()
-  }
+    const jwtResponse = await verifySessionCookie(token, secret);
+    if (!jwtResponse.payload) {
+        return new UnauthorizedAPIRequest();
+    }
 
-  const body =
-    request.json &&
-    ((await request.json()) as {
-      email: string
-      currentPassword: string
-      newPassword: string
-      signoutOnUpdate?: boolean | undefined
-    })
+    const body =
+        request.json &&
+        ((await request.json?.()) as {
+            email: string;
+            currentPassword: string;
+            newPassword: string;
+            signoutOnUpdate?: boolean | undefined;
+        });
 
-  if (!body?.email || !body?.currentPassword || !body?.newPassword) {
-    return new InvalidRequestBodyError()
-  }
+    if (!body?.email || !body?.currentPassword || !body?.newPassword) {
+        return new InvalidRequestBodyError();
+    }
 
-  const { docs } = await payload.find({
-    collection: internal.usersCollectionSlug,
-    where: {
-      email: { equals: body.email },
-    },
-    limit: 1,
-  })
+    const { docs } = await payload.find({
+        collection: internal.usersCollectionSlug,
+        where: {
+            email: { equals: body.email },
+        },
+        limit: 1,
+    });
 
-  if (docs.length !== 1) {
-    return new UserNotFoundAPIError()
-  }
+    if (docs.length !== 1) {
+        return new UserNotFoundAPIError();
+    }
 
-  const user = docs[0]
-  const isVerifed = await verifyPassword(
-    body.currentPassword,
-    user["hashedPassword"],
-    user["salt"],
-    user["hashIterations"],
-  )
-  if (!isVerifed) {
-    return new InvalidCredentials()
-  }
+    const user = docs[0];
+    const isVerifed = await verifyPassword(
+        body.currentPassword,
+        user["hashedPassword"],
+        user["salt"],
+        user["hashIterations"],
+    );
+    if (!isVerifed) {
+        return new InvalidCredentials();
+    }
 
-  const {
-    hash: hashedPassword,
-    salt,
-    iterations,
-  } = await hashPassword(body.newPassword)
+    const { hash: hashedPassword, salt, iterations } = await hashPassword(body.newPassword);
 
-  await payload.update({
-    collection: internal.usersCollectionSlug,
-    id: user.id,
-    data: {
-      hashedPassword,
-      salt,
-      hashIterations: iterations,
-    },
-  })
+    await payload.update({
+        collection: internal.usersCollectionSlug,
+        id: user.id,
+        data: {
+            hashedPassword,
+            salt,
+            hashIterations: iterations,
+        },
+    });
 
-  if (!!body.signoutOnUpdate) {
-    let cookies: string[] = []
-    cookies = [...invalidateSessionCookies(cookieName, cookies)]
-    return revokeSession(cookies)
-  }
+    if (!!body.signoutOnUpdate) {
+        let cookies: string[] = [];
+        cookies = [...invalidateSessionCookies(cookieName, cookies)];
+        return revokeSession(cookies);
+    }
 
-  const res = new Response(
-    JSON.stringify({
-      message: "Password reset complete",
-      kind: SuccessKind.Updated,
-      isSuccess: true,
-      isError: false,
-    }),
-    {
-      status: 201,
-    },
-  )
-  return res
-}
+    const res = new Response(
+        JSON.stringify({
+            message: "Password reset complete",
+            kind: SuccessKind.Updated,
+            isSuccess: true,
+            isError: false,
+        }),
+        {
+            status: 201,
+        },
+    );
+    return res;
+};
