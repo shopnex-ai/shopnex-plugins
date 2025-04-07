7f2db0bdb675b91ec778d681edb1e6283effc168
ArseniyDev <jkl@usa.com>
2025-04-07 17:25:12 +0300

    remove: useless app file of oauth plugin

--- auth-plugin/src/index.ts
+++ auth-plugin/src/index.ts
@@ -1,4 +1,3 @@
 import { adminAuthPlugin } from "./plugins/admin"
-import { appAuthPlugin } from "./plugins/app"
 import { authPlugin } from "./plugins/auth"
-export { adminAuthPlugin, appAuthPlugin, authPlugin }
+export { adminAuthPlugin, authPlugin }
