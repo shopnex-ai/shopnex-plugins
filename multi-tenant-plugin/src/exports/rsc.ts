02226bdb52a8793c56178d03002e6e130a582593
ArseniyDev <jkl@usa.com>
2025-05-18 12:27:23 +0300

    feat: add GlobalRedirectClient component and update exports in rsc.ts

--- multi-tenant-plugin/src/exports/rsc.ts
+++ multi-tenant-plugin/src/exports/rsc.ts
@@ -1,2 +1,3 @@
-export { GlobalViewRedirect } from "../components/GlobalViewRedirect/index";
+export { GlobalViewRedirect } from "../components/GlobalViewRedirect";
 export { TenantSelectionProvider } from "../providers/TenantSelectionProvider/index";
+export { EmptyList } from "@shopnex/utils/rsc";
