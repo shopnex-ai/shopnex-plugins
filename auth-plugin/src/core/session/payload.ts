1e1387c7181e99520d8a1b11a287c9bb61cbeb06
ArseniyDev <jkl@usa.com>
2025-06-02 20:14:45 +0300

    feat: mark user as verified when creating a new account in PayloadSession

--- auth-plugin/src/core/session/payload.ts
+++ auth-plugin/src/core/session/payload.ts
@@ -54,6 +54,7 @@ export class PayloadSession {
                 disableVerificationEmail: true,
                 data: {
                     email: accountInfo.email,
+                    _verified: true,
                     shops: [
                         {
                             shop: newShop.id,
